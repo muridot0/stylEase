3c8a56e3ea7ca0b293d372be27365d7c3c809718
muridot0 <muri0@outlook.com>
2024-04-14 16:19:00 +0100

    added ui for sidebar

--- client/app/components/NodeDrawer.tsx
+++ client/app/components/NodeDrawer.tsx
@@ -15,21 +15,43 @@ export default function NodeDrawer() {
   return (
     <Panel
       position='top-left'
-      className='flex items-center top-[25rem] rounded-[8px] bg-[--node-bg-color] border border-[--node-border-color] p-4'
+      className={clsx(
+        'flex items-center top-[25rem] rounded-[8px] bg-[--node-bg-color] border border-[--node-border-color] p-4',
+        { 'w-75': !collapsed }
+      )}
       style={{ top: 150 }}
     >
-      <ul className='flex'>
-        {nodeData.map((data) => (
-          <li className='[&:not(:last-child)]:mr-4' key={data.id}>
-            <button className='flex p-3 border border-[--node-border-color] rounded-[4px] hover:bg-[--hover-bg-color] hover:text-[--hover-color]'>
-              <span
-                className={clsx(`${data.icon} text-3xl flex`, {
-                  'i-lucide-brain-cog': data.icon === 'i-lucide-brain-cog'
-                })}
-              ></span>
-            </button>
-          </li>
-        ))}
+      <ul
+        className={clsx(
+          'w-full',
+          { flex: collapsed },
+          {
+            block: !collapsed
+          }
+        )}
+      >
+        {collapsed
+          ? nodeData.map((data) => (
+              <li className='[&:not(:last-child)]:mr-4' key={data.id}>
+                <button className='flex p-3 border border-[--node-border-color] rounded-[4px] hover:bg-[--hover-bg-color] hover:text-[--hover-color]'>
+                  <span
+                    className={clsx(`${data.icon} text-3xl flex`, {
+                      'i-lucide-brain-cog': data.icon === 'i-lucide-brain-cog'
+                    })}
+                  ></span>
+                </button>
+              </li>
+            ))
+          : nodeData.map((data) => (
+              <li className='border border-[--node-border-color] rounded-[4px] [&:not(:last-child)]:mb-4 p-2 hover:bg-[--hover-bg-color] hover:text-[--hover-color]'>
+                <div className='flex items-center justify-between text-xl'>
+                  <p>{data.title}</p>
+                  <span className={clsx(`${data.icon} flex`)}></span>
+                </div>
+
+                <p className='text-[--node-icons-color]'>{data.description}</p>
+              </li>
+            ))}
       </ul>
       <button
         className='bg-[--node-bg-color] border rounded-full border-[--node-border-color] absolute -right-10 top-0 p-1'
