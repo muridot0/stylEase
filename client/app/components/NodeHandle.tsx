bf54a26c163038624fecde99b88c2780d731639f
muridot0 <muri0@outlook.com>
2024-04-15 07:32:12 +0100

    adding node delete funcitonality

--- client/app/components/NodeHandle.tsx
+++ client/app/components/NodeHandle.tsx
@@ -13,7 +13,7 @@ function NodeHandle({ className, ...props }: Props) {
       className={clsx(
         '!transform-none',
         {
-          '!left-[0.1rem] !rounded-none !border-t-4 !border-b-4 !border-l-8 !border-t-transparent !border-b-transparent !border-l-[--node-handle-color] no-bg': props.position !== Position.Right,
+          '!left-0 !rounded-none !border-t-4 !border-b-4 !border-l-8 !border-t-transparent !border-b-transparent !border-l-[--node-handle-color] no-bg': props.position !== Position.Right,
           '!right-[-0.125rem] !border-none !w-[7px] !h-[7px] !top-[1.65rem] z-2 !bg-[--node-handle-color]': props.position === Position.Right
         },
         className
--- client/app/components/WrapperNode.tsx
+++ client/app/components/WrapperNode.tsx
@@ -1,6 +1,7 @@
 import clsx from 'clsx'
 import NodeHandle from './NodeHandle'
-import { Position} from 'reactflow'
+import { Position } from 'reactflow'
+import React from 'react'
 
 interface Props extends React.PropsWithChildren {
   nodeTitle: string
@@ -8,39 +9,77 @@ interface Props extends React.PropsWithChildren {
   iconClass?: string
   position: Position
   selected: boolean
+  onHover: (id: string) => void
 }
 
-function WrapperNode({ children, className, iconClass, nodeTitle, position, selected }: Props) {
+function WrapperNode({
+  children,
+  className,
+  iconClass,
+  nodeTitle,
+  position,
+  selected,
+  onHover
+}: Props) {
+  const [hovering, setHovering] = React.useState(false)
+
   //TODO: add functionality to delete node (should show delete icon on hover)
   const renderHandles = () => {
-    switch(position) {
+    switch (position) {
       case 'left':
-        return <NodeHandle type="target" position={Position.Left}/>
+        return <NodeHandle type='target' position={Position.Left} />
       case 'right':
-        return <NodeHandle id='a' type="source" position={Position.Right} />
+        return <NodeHandle id='a' type='source' position={Position.Right} />
       default:
-        return null;
+        return null
     }
+  }
+
+  const handleHover = () => {
+    setHovering(!hovering)
+  }
+
+  const handleStopHovering = () => {
+    setHovering(!hovering)
   }
 
   return (
     <div
-      className={clsx(
-        className,
-        'w-full min-w-[200px] bg-[--node-bg-color] border border-[--node-border-color] py-6 px-10 rounded-[8px] text-center',
-        { 'dark:border-[#b3b3b3] light:border-[#808080]': selected }
-      )}
+      className='flex flex-col items-end'
+      onMouseEnter={handleHover}
+      onMouseLeave={handleStopHovering}
     >
-      <section className='flex items-center gap-2 mb-4 justify-center'>
-        {iconClass && (
-          <span className={clsx(iconClass, 'flex text-[--node-icons-color]')} />
+      <div
+        className={clsx(
+          className,
+          'relative w-full min-w-[200px] bg-[--node-bg-color] border border-[--node-border-color] py-6 px-10 rounded-[8px] text-center',
+          { 'dark:border-[#b3b3b3] light:border-[#808080]': selected }
         )}
-        <h1>{nodeTitle}</h1>
-      </section>
+      >
+        <section className='flex items-center gap-2 mb-4 justify-center'>
+          {iconClass && (
+            <span
+              className={clsx(iconClass, 'flex text-[--node-icons-color]')}
+            />
+          )}
+          <h1>{nodeTitle}</h1>
+        </section>
 
-      {renderHandles()}
+        {renderHandles()}
 
-      {children}
+        {children}
+      </div>
+
+      {hovering && (
+        <div className='flex gap-2 w-fit border border-[--node-border-color] mt-1 bg-[--node-bg-color] rounded-[8px] px-2 py-1'>
+          <button className='flex border border-[--node-border-color] p-1 rounded-1 hover:bg-[--hover-bg-color] hover:text-[--hover-color]'>
+            <span className='i-lucide-unlink text-xs'></span>
+          </button>
+          <button className='flex border border-[--node-border-color] p-1 rounded-1 hover:bg-[--hover-bg-color] hover:text-[--hover-color]'>
+            <span className='i-lucide-trash-2 text-xs'></span>
+          </button>
+        </div>
+      )}
     </div>
   )
 }
