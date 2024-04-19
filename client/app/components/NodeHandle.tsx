2cf492a5a622e7980e6055c3b990b022a08863ef
muridot0 <muri0@outlook.com>
2024-04-19 07:50:49 +0100

    added handle validation

--- client/app/components/NodeHandle.tsx
+++ client/app/components/NodeHandle.tsx
@@ -1,20 +1,18 @@
 import clsx from 'clsx'
-import React from 'react'
-import {
-  Handle,
-  Position,
-  useStore,
-  useReactFlow
-} from 'reactflow'
-import type { Connection, HandleProps, ReactFlowState } from 'reactflow'
+import { Handle, Position, useStore } from 'reactflow'
+import type { Connection, HandleProps } from 'reactflow'
 
 interface Props extends HandleProps {
   className?: string
 }
 
 function NodeHandle({ className, ...props }: Props) {
-  const isSourceConnected = useStore(s => s.edges.some((edge) => edge.source === props.id))
-  const isTargetConnected = useStore(s => s.edges.some((edge) => edge.target === props.id))
+  const isSourceConnected = useStore((s) =>
+    s.edges.some((edge) => edge.source === props.id)
+  )
+  const isTargetConnected = useStore((s) =>
+    s.edges.some((edge) => edge.target === props.id)
+  )
   console.log(isSourceConnected)
 
   function isValidHandle(connection: Connection) {
@@ -35,9 +33,9 @@ function NodeHandle({ className, ...props }: Props) {
         className
       )}
       isValidConnection={isValidHandle}
-      isConnectableStart={isSourceConnected ? false : true}
-      isConnectableEnd={isTargetConnected ? false : true}
-      // isConnectable={isHandleConnectable}
+      isConnectableStart={
+        props.type === 'target' ? false : isSourceConnected ? false : true
+      }
     ></Handle>
   )
 }
