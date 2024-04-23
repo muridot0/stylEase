341c215cf8149a2c1fe4290ef31e5f3b1c923286
muridot0 <muri0@outlook.com>
2024-04-23 07:17:19 +0100

    checkpoint: adding context menu

--- client/app/components/Header.tsx
+++ client/app/components/Header.tsx
@@ -28,7 +28,24 @@ export default function Header() {
         >
           <span className='i-lucide-menu flex text-2xl' />
         </button>
-        <div className={clsx('absolute mt-2 right-8 border bg-[--node-bg-color] border-[--node-border-color] w-40 h-40 p-4 rounded-[8px] transition-opacity ease-in-out', {'opacity-100': toggleMenu}, {'opacity-0': !toggleMenu} )}>here i am </div>
+        <ul className={clsx('absolute mt-2 right-8 border bg-[--node-bg-color] border-[--node-border-color] w-max h-max p-4 rounded-[8px] transition-opacity ease-in-out', {'opacity-100': toggleMenu}, {'opacity-0': !toggleMenu})}>
+          <div>
+            <h2>Background Variant</h2>
+            <li className='font-light'>Dots</li>
+            <li className='font-light'>Cross</li>
+            <li className='font-light'>lines</li>
+          </div>
+          <div className='mt-4'>
+            <h2>Actions</h2>
+            <li className='font-light'>Clear nodes</li>
+            <li className='font-light'>Clear connections</li>
+          </div>
+          <div className='mt-4'>
+            <h2>Controls</h2>
+            <li className='font-light'>Dots</li>
+            <li className='font-light'>Cross</li>
+          </div>
+        </ul>
       </div>
     </div>
   )
