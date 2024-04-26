8cf2fd6bbe07c5e5fa4fb6d23ec643e0768a92d5
muridot0 <muri0@outlook.com>
2024-04-26 07:24:39 +0100

    added click on outside function for context menu

--- client/app/components/Header.tsx
+++ client/app/components/Header.tsx
@@ -1,16 +1,15 @@
-import clsx from 'clsx'
 import React from 'react'
 import ContextMenu from './ContextMenu'
 
 export default function Header() {
   const [toggleMenu, setToggleMenu] = React.useState(false)
-  const contextRef = React.useRef<HTMLDivElement>()
+  const contextRef = React.useRef<HTMLDivElement>(null)
 
   const showMenu = (): void => setToggleMenu(!toggleMenu)
 
   React.useEffect(() => {
-    const handleClickOutside = (e) => {
-      if (contextRef.current && !contextRef.current.contains(e.target)) {
+    const handleClickOutside = (e: MouseEvent) => {
+      if (contextRef.current && !contextRef.current.contains(e.target as HTMLElement)) {
         setToggleMenu(false)
       }
     }
@@ -34,13 +33,6 @@ export default function Header() {
         </picture>
         <h2 className='mt-1 text-xl font-bold'>stylEase</h2>
       </div>
-      {/*
-        <button
-          className='border bg-[--node-bg-color] border-[--node-border-color] hover:bg-[--hover-bg-color] hover:text-[--hover-color] p-2 rounded-[50%]'
-          onClick={showMenu}
-        >
-          <span className='i-lucide-menu flex text-2xl' />
-        </button> */}
       <ContextMenu
         toggleMenu={toggleMenu}
         showMenu={showMenu}
