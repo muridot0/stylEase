0bd84bd93747483172f6be872a6335553cd74bc9
muridot0 <muri0@outlook.com>
2024-06-10 08:22:03 +0100

    added og content and removed unused font and images

--- home/astro.config.mjs
+++ home/astro.config.mjs
@@ -4,5 +4,6 @@ import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://muri-o.com/stylease",
   integrations: [react(), UnoCSS({ injectReset: true })]
 })
